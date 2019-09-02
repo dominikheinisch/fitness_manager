127edff0d3ffdd945dad84cf9de7f24c8c979e45
dominik <dominikheinisch2@gmail.com>
2019-09-02 19:00:48 +0200

    added User to model

--- fitness_app/models.py
+++ fitness_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    birth_date = models.DateField('birth date')
+
