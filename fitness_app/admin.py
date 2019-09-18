cd73a97802894073a1c17e30bfb7318eeb046735
dominik <dominikheinisch2@gmail.com>
2019-09-18 10:27:21 +0200

    skeleton of db + dummy data filler

--- fitness_app/admin.py
+++ fitness_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import Activity, User, Sport
 
+admin.site.register(Activity)
 admin.site.register(User)
+admin.site.register(Sport)
--- fitness_app/models.py
+++ fitness_app/models.py
@@ -5,4 +5,53 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     birth_date = models.DateField('birth date')
+    MALE = 'M'
+    FEMALE = 'F'
+    gender = models.CharField(
+        max_length=1,
+        choices=[(MALE, 'male'), (FEMALE, 'female')],
+        default=None,
+        null=True,
+    )
 
+
+class Sport(models.Model):
+    name = models.CharField(max_length=50)
+    calories_per_hour = models.CharField(max_length=50)
+
+
+class Activity(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    Sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    start = models.DateTimeField('start time')
+    stop = models.DateTimeField('stop time')
+
+
+def fill_default_database():
+    for user in [
+        User(first_name='jan', surname='nowak', birth_date='1999-9-9'),
+        User(first_name='dominik', surname='nowak', birth_date='1999-9-9'),
+        User(first_name='michal', surname='kowalski', birth_date='2000-1-1'),
+    ]:
+        user.save()
+
+    for sport in [
+        Sport(name='cycling', calories_per_hour=62),
+        Sport(name='running', calories_per_hour=81),
+        Sport(name='gym', calories_per_hour=112),
+    ]:
+        sport.save()
+
+    for activity in [
+        Activity(User=User.objects.get(id=1), Sport=Sport.objects.get(id=1), start='1999-9-9T13:00',
+                 stop='1999-9-10T13:15'),
+        Activity(User=User.objects.get(id=1), Sport=Sport.objects.get(id=2), start='2019-9-9T13:00',
+                 stop='2019-9-10T13:15'),
+        Activity(User=User.objects.get(id=1), Sport=Sport.objects.get(id=3), start='2019-9-9', stop='2019-9-10'),
+        Activity(User=User.objects.get(id=2), Sport=Sport.objects.get(id=2), start='2019-9-9', stop='2019-9-10'),
+        Activity(User=User.objects.get(id=3), Sport=Sport.objects.get(id=3), start='2019-9-11', stop='2019-9-11'),
+    ]:
+        activity.save()
+
+
+# fill_default_database()
