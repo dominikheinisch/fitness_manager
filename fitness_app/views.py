9afd7dd359c1bd9ce2cb978a39ba49b7967426a6
dominik <dominikheinisch2@gmail.com>
2019-09-23 11:05:43 +0200

    created dummy index

--- fitness_app/views.py
+++ fitness_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import User
+
+
+def index(request):
+    user_list = User.objects.order_by('birth_date')[::-1]
+    template = loader.get_template('index.html')
+    context = {
+        'user_list': user_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, user_id):
+    return HttpResponse("You're looking at User %s." % user_id)
