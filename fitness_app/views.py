935b3cf9634ce371b10f30002c319764e3bd7b98
dominik <dominikheinisch2@gmail.com>
2019-11-30 04:00:20 +0100

    added image and details page

--- fitness_app/views.py
+++ fitness_app/views.py
@@ -1,20 +1,28 @@
 from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import get_object_or_404
-from django.template import loader
+from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404, render
 
-from .models import User
+from .models import Activity, User
 
 
 def index(request):
     user_list = User.objects.order_by('birth_date')[::-1]
-    template = loader.get_template('index.html')
     context = {
         'user_list': user_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def detail(request, user_id):
     user = get_object_or_404(User, pk=user_id)
-    return HttpResponse("You're looking at User %s." % user)
+    # return HttpResponse("You're looking at User %s." % user)
+
+    # activity = Activity.objects.select_related('User').get(id=user_id)            # return only 1
+    # activities = Activity.objects.prefetch_related('User').filter(User_id=user_id)
+    activities = Activity.objects.filter(User_id=user_id)
+    a = activities[0]
+    context = {
+        'user': user,
+        'activities': activities,
+    }
+    return render(request, 'detail.html', context)
