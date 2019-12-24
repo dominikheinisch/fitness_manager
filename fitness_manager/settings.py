22341223dbe4800abe85abab2ec4fd030717a38b
dominik <dominikheinisch2@gmail.com>
2019-12-25 00:36:24 +0100

    disabled timezone USE_TZ

--- fitness_manager/settings.py
+++ fitness_manager/settings.py
@@ -107,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Warsaw'
+# TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
