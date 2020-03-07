5f2074c74accd3a988fa97bff1429a8e9678d44e
dominik <dominikheinisch2@gmail.com>
2020-03-08 00:17:35 +0100

    tests: added setUp to ut's

--- fitness_app/tests/tests_ut.py
+++ fitness_app/tests/tests_ut.py
@@ -10,8 +10,11 @@ from ..views import get_meals_data, get_portions_by_meal_id, get_summary_of_cons
 class QueryTests(TestCase):
     fixtures = ['default_db.yaml']
 
+    def setUp(self):
+        self.request = self.get_default_request()
+
     def test_get_portions_by_meal_id(self):
-        portions = get_portions_by_meal_id(request=self.get_default_request(), meal_id=1)
+        portions = get_portions_by_meal_id(request=self.request, meal_id=1)
         self.assertEquals(len(portions), 1)
         portion = portions[0]
         self.assertEquals(portion.Food.__str__(), 'rice')
@@ -19,18 +22,18 @@ class QueryTests(TestCase):
 
     def test_empty_get_summary_of_consume(self):
         date = datetime.strptime('1900-01-01', '%Y-%m-%d')
-        daily_totals = get_summary_of_consume(request=self.get_default_request(), date=date)
+        daily_totals = get_summary_of_consume(request=self.request, date=date)
         self.assertEquals(list(daily_totals), [0] * 4)
 
     def test_get_summary_of_consume(self):
         date = datetime.strptime('2020-01-01', '%Y-%m-%d')
-        daily_totals = get_summary_of_consume(request=self.get_default_request(), date=date)
+        daily_totals = get_summary_of_consume(request=self.request, date=date)
         self.assertEquals(list(daily_totals), [546, 21, 176, 7])
 
     def test_get_meals_data(self):
         from_date = datetime.strptime('1900-01-01', '%Y-%m-%d')
         to_date = datetime.strptime('2100-01-01', '%Y-%m-%d')
-        meals_data = get_meals_data(request=self.get_default_request(), from_date=from_date, to_date=to_date)
+        meals_data = get_meals_data(request=self.request, from_date=from_date, to_date=to_date)
         result = [
             {'count': 1, 'date': '2020-02-02', 'day_calories': 2000},
             {'count': 2, 'date': '2020-01-01', 'day_calories': 546},
